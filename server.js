408150e3123523e0770afd884c2ed86f1c53fa7a
Gowri239 <gowrimopuru14@gmail.com>
2022-10-02 15:25:17 +0530

    url hits

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
 const http = require('http');
 const server = http.createServer((req,res) => {
-    console.log('Gowri');
+    const url = req.url;
+    if(url === '/node'){
+        res.write('<html>')
+        res.write('<head><title>project page</title></head>')
+        res.write('<body><h1>Welcome to my Node js project</h1></body>')
+        res.write('</html>')
+        res.end()
+    }
+    if(url === '/home'){
+        res.write('<html>')
+        res.write('<head><title>project page</title></head>')
+        res.write('<body><h1>Welcome home</h1></body>')
+        res.write('</html>')
+        res.end()
+    }
+    if(url === '/about'){
+        res.write('<html>')
+        res.write('<head><title>project page</title></head>')
+        res.write('<body><h1>Welcome to about us page</h1></body>')
+        res.write('</html>')
+        res.end()
+    }
+    
 })
 server.listen(4000);
