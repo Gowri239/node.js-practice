be28613527f1ff0277118b5fb1ccb0d374720156
Gowri239 <gowrimopuru14@gmail.com>
2022-10-02 12:59:57 +0530

    createServer

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const http = require('http')
+const http = require('http');
 const server = http.createServer((req,res) => {
-    console.log('Gowri')
+    console.log('Gowri');
 })
-server.listen(4000)
+server.listen(4000);
