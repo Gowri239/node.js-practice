12e052c0246f28bab22ed77b62816effd57ef1c9
Gowri239 <gowrimopuru14@gmail.com>
2022-10-02 20:51:33 +0530

    express start

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
-const routes = require('./routes')
+const express = require('express')
 
-//const server = http.createServer(routes)
-const server = http.createServer(routes.handler)
-server.listen(4000);
+const app = express()
+
+app.use((req,res,next) => {
+    console.log("In the middleware")
+    next()
+})
+
+app.use((req,res,next) => {
+    res.send('<h1>Hello from Node.js</h1>')
+})
+app.listen(4000)
